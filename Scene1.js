df11cde3525c91d592e97e74a08c44210b84f95d
ipaullly <ipaully22@gmail.com>
2020-01-26 00:24:48 +0300

    animate spritesheets

--- Scene1.js
+++ Scene1.js
@@ -5,9 +5,22 @@ class Scene1 extends Phaser.Scene {
 
   preload() {
     this.load.image("background", "assets/background.png");
-    this.load.image("ship", "assets/ship.png");
-    this.load.image("ship2", "assets/ship2.png");
-    this.load.image("ship3", "assets/ship3.png");
+    this.load.spritesheet("explosion", "spritesheets/explosion.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+    this.load.spritesheet("ship", "spritesheets/ship.png", {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+    this.load.spritesheet("ship2", "spritesheets/ship2.png", {
+      frameWidth: 32,
+      frameHeight: 16,
+    });
+    this.load.spritesheet("ship3", "spritesheets/ship3.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
   }
 
   create() {
--- Scene2.js
+++ Scene2.js
@@ -6,9 +6,42 @@ class Scene2 extends Phaser.Scene {
     this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
     this.background.setOrigin(0, 0);
 
-    this.ship1 = this.add.image(config.width/2 - 50, config.height/2, "ship");
-    this.ship2 = this.add.image(config.width/2, config.height/2, "ship2");
-    this.ship3 = this.add.image(config.width/2 + 50, config.height/2, "ship3");
+    this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship");
+    this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2");
+    this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
+
+    this.anims.create({
+      key: "ship1_anim",
+      frames: this.anims.generateFrameNumbers("ship"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "ship2_anim",
+      frames: this.anims.generateFrameNumbers("ship2"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "ship3_anim",
+      frames: this.anims.generateFrameNumbers("ship3"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    // this.anims.create({
+    //   key: "explode",
+    //   frames: this.anims.generateFrameNumbers("explosion"),
+    //   frameRate: 20,
+    //   repeat: 0,
+    //   hideOnComplete: true
+    // });
+
+    this.ship1.play("ship1_anim", true);
+    this.ship2.play("ship2_anim", true);
+    this.ship3.play("ship3_anim", true);
 
     this.add.text(20, 20, "Playing Game", { 
       font: "25px Arial",
